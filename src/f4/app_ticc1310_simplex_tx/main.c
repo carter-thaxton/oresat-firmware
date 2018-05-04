f84c0ef615760c9b5348caa24e6431337932a7fc
Malay <malaydasat@gmail.com>
2018-05-03 22:41:42 -0700

    TI CC1310 radio send and receive

---
+++ src/f4/app_ticc1310_simplex_tx/main.c
@@ -0,0 +1,171 @@
+/*
+    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/***************************************************
+ *     Modification Log
+ *     05/03/2018    Malay Das    Initial Code.  
+ ***************************************************/
+
+
+/*
+ * Include Files
+ */
+#include <stdbool.h>
+#include "ch.h"
+#include "hal.h"
+#include "chprintf.h"
+#include "util_version.h"
+#include "util_numbers.h"
+
+#define     DEBUG_SERIAL                    SD2
+#define     DEBUG_CHP                       ((BaseSequentialStream *) &DEBUG_SERIAL)
+
+
+/*
+ * Serial Driver Configuration
+ */
+static SerialConfig ser_cfg =
+{
+    115200,                                 //Baud rate
+    0,                                      //
+    0,                                      //
+    0,                                      //
+};
+
+
+/*
+ * Receive and Transmit SPI Configurations
+ */
+static const SPIConfig spicfg_rx =
+{
+    false,
+    NULL,                                   // Operation complete callback
+    GPIOA,                                  // Slave select port
+    GPIOA_SPI1_NSS,                         // Slave select pad
+    // SPI cr1 data                            (see 446 ref man.)
+    SPI_CR1_SPE     |                       // SPI enable
+    SPI_CR1_MSTR    |                       // Master
+    //SPI_CR1_BR_2    |
+    SPI_CR1_BR_1    |
+    SPI_CR1_BR_0   |                        // fpclk/16  approx 5Mhz? BR = 0x011
+    SPI_CR1_SSM,
+    0, // SPI_CR2_SSOE,
+};
+
+static const SPIConfig spicfg_tx =
+{
+    false,
+    NULL,                                   // Operation complete callback
+    GPIOB,                                  // Slave select port
+    GPIOB_SPI2_NSS,                         // Slave select pad
+    // SPI cr1 data                         (see 446 ref man.)
+    SPI_CR1_SPE     |                       // SPI enable
+    SPI_CR1_MSTR    |                       // Master
+    //SPI_CR1_BR_2    |
+    SPI_CR1_BR_1    |
+    SPI_CR1_BR_0   |                        // fpclk/16  approx 5Mhz? BR = 0x011
+    SPI_CR1_SSM,
+    0, // SPI_CR2_SSOE,
+};
+
+
+/*
+ * Initialize the SPI drivers and configure the adf7030 chips
+ */
+static void app_init(void)
+{
+    // Start up debug output, chprintf(DEBUG_CHP,...)
+    sdStart(&DEBUG_SERIAL, &ser_cfg);
+
+    set_util_fwversion(&version_info);
+    set_util_hwversion(&version_info);
+
+
+    //Print FW/HW information
+    chprintf(DEBUG_CHP, "\r\nFirmware Info\r\n");
+    chprintf(DEBUG_CHP, "FW HASH: %s\r\n", version_info.firmware);
+    chprintf(DEBUG_CHP, "STF0x UNIQUE HW ID (H,C,L):\r\n0x%x\t0x%x\t0x%x\r\n"
+             , version_info.hardware.id_high
+             , version_info.hardware.id_center
+             , version_info.hardware.id_low
+            );
+		
+	
+    spiStart(&SPID1, &spicfg_rx);
+    spiStart(&SPID2, &spicfg_tx);
+	//spiSelect(&SPID2); 
+
+
+    chprintf(DEBUG_CHP, "Sending Packets via ti-cc1310\r\n");
+
+
+	while (true)
+    {
+      chThdSleepMilliseconds(1000);
+      uint8_t tx_buf[64]={0x01,0x01,1,1,1,6,7,8,9,0,1,2,3,4,5,6,7,8,9};
+
+	  spiSelect(&SPID1);
+	  spiStartSend(&SPID1, 64, &tx_buf);
+	  while(SPID1.state != SPI_READY) {}
+
+	  spiUnselect(&SPID1);
+      chprintf(DEBUG_CHP, "Sent a packet\r\n");
+    }
+
+}
+
+
+/*
+ * main loop blinks the led
+ */
+static void main_loop(void)
+{   
+    chThdSleepMilliseconds(500);
+
+
+	while (true)
+    {
+        chThdSleepMilliseconds(500);
+        chprintf(DEBUG_CHP, ".");
+		palTogglePad(GPIOA, GPIOA_SX_TESTOUT);
+    }
+
+}
+
+
+/*
+ * Entry to our code
+ */
+int main(void)
+{
+    halInit();
+    chSysInit();
+    app_init();
+
+    // Enabling events on both edges of the button line.*/
+    //palEnableLineEvent(GPIOC_SX_DIO3, PAL_EVENT_MODE_RISING_EDGES);
+
+	//chThdCreateStatic(waThread_sx1236_rx,      sizeof(waThread_sx1236_rx),   NORMALPRIO, Thread_sx1236_rx, NULL);
+    //chThdSleepMilliseconds(500);
+    //chThdCreateStatic(waThread_sx1236_tx,      sizeof(waThread_sx1236_tx),   NORMALPRIO, Thread_sx1236_tx, NULL);
+    chThdSleepMilliseconds(500);
+
+    main_loop();
+    return 0;
+}
+
+
+
